6f471940fd10f7288b9426aaa2fd6fecc9074983
Sonia Gisel <sogispe@gmail.com>
2014-08-26 15:08:16 -0300

    Almost finished the guide, all that remains for me it's to complete the task num 15

--- users_table.sql
+++ users_table.sql
@@ -1,7 +1,7 @@
 USE globant;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-	id INT NOT NULL AUTO_INCREMENT  PRIMARY KEY,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	first_name VARCHAR (50) NOT NULL,
 	last_name VARCHAR (50) NOT NULL,
 	sex ENUM ("male", "female") NOT NULL,
@@ -19,5 +19,26 @@ INSERT INTO users (first_name, last_name, sex, age) VALUES ("Dante", "Pregal", "
 INSERT INTO users (first_name, last_name, sex, age) VALUES ("Bruno", "Yorda", "male", 24);
 INSERT INTO users (first_name, last_name, sex, age) VALUES ("Pedro", "Pereyra", "male", 26);
 
-#SELECT * FROM users;
-SELECT * FROM users WHERE age>=25;
+SELECT * FROM users;
+#SELECT * FROM users WHERE age>=25;
+#SELECT * FROM users WHERE first_name LIKE "A%";
+#SELECT * FROM users WHERE upper(first_name) LIKE "A%" and sex="male";
+#DELETE FROM users WHERE age=null;
+
+DROP TABLE IF EXISTS locations;
+CREATE TABLE locations (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	first_name VARCHAR (50) NOT NULL,
+	last_name VARCHAR (50) NOT NULL,
+	location ENUM ("North Park", "South Park", "Alsina", "Laminar") NOT NULL
+);
+
+INSERT INTO locations (first_name, last_name, location) VALUES ("Carolina", "Santucho", "North Park");
+INSERT INTO locations (first_name, last_name, location) VALUES ("Emanuel", "Pérez", "South Park");
+INSERT INTO locations (first_name, last_name, location) VALUES ("Ricardo", "Molina", "Laminar");
+INSERT INTO locations (first_name, last_name, location) VALUES ("Felipe", "Miceli", "South Park");
+INSERT INTO locations (first_name, last_name, location) VALUES ("Fernanda", "Gonzalez", "North Park");
+
+SELECT * FROM locations;
+#TRUNCATE TABLE locations;
+#DROP TABLE locations;
