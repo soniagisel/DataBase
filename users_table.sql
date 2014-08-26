ac8c4939bfb7deb16e01654496a2e05521c9b494
Sonia Gisel <sogispe@gmail.com>
2014-08-26 11:51:19 -0300

    I've completed the guide 'till the task num 12, keep working on the 13

--- users_table.sql
+++ users_table.sql
@@ -1,7 +1,23 @@
-create table users (
-	id auto_increment NOT NULL primary key,
-	first_name varchar 50 NOT NULL,
-	last_name varchar 50 NOT NULL,
-	sex enum "male", "female" NOT NULL,
-	age init 3 NULL
+USE globant;
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+	id INT NOT NULL AUTO_INCREMENT  PRIMARY KEY,
+	first_name VARCHAR (50) NOT NULL,
+	last_name VARCHAR (50) NOT NULL,
+	sex ENUM ("male", "female") NOT NULL,
+	age INT (3) NULL
 );
+
+INSERT INTO users (first_name, last_name, sex, age) VALUES ("Carolina", "Santucho", "female", 26);
+INSERT INTO users (first_name, last_name, sex, age) VALUES ("Emanuel", "Pérez", "male", 40);
+INSERT INTO users (first_name, last_name, sex, age) VALUES ("Ricardo", "Molina", "male", 29);
+INSERT INTO users (first_name, last_name, sex, age) VALUES ("Felipe", "Miceli", "male", 22);
+INSERT INTO users (first_name, last_name, sex, age) VALUES ("Fernanda", "Gonzalez", "female", 31);
+INSERT INTO users (first_name, last_name, sex, age) VALUES ("Andrea", "Zamora", "female", 27);
+INSERT INTO users (first_name, last_name, sex, age) VALUES ("Juan", "Suarez", "male", 25);
+INSERT INTO users (first_name, last_name, sex, age) VALUES ("Dante", "Pregal", "male", 24);
+INSERT INTO users (first_name, last_name, sex, age) VALUES ("Bruno", "Yorda", "male", 24);
+INSERT INTO users (first_name, last_name, sex, age) VALUES ("Pedro", "Pereyra", "male", 26);
+
+#SELECT * FROM users;
+SELECT * FROM users WHERE age>=25;
